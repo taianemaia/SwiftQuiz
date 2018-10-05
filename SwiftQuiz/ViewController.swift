60815e26bf80cf747d014c8bce225322e8c1d03f
user912249 <user912249@74-80-245-13.ic2net.net>
2018-10-05 16:12:42 -0700

    Initial Commit

---
+++ SwiftQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftQuiz
+//
+//  Created by user912249 on 10/5/18.
+//  Copyright © 2018 Taiane Maia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
